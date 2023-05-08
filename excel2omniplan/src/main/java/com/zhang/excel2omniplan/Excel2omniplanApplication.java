7241d74da94a0e57f12cedeac556a31ec45c6b72
zhangwei <zhangwei13@fanhuaholdings.com>
2023-05-08 10:32:35 +0800

    基础架构

---
+++ excel2omniplan/src/main/java/com/zhang/excel2omniplan/Excel2omniplanApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.excel2omniplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Excel2omniplanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Excel2omniplanApplication.class, args);
+    }
+
+}
---
+++ excel2omniplan/src/test/java/com/zhang/excel2omniplan/Excel2omniplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhang.excel2omniplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Excel2omniplanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
